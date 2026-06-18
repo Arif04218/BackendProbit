f254e47269c96f750be0d7311b868688c5a3b002
Arif Rahman Hakim <162030147+Arif04218@users.noreply.github.com>
2026-06-19 06:25:27 +0700

    Remove server listen log from server.js
    Removed server listening log statement.

--- server.js
+++ server.js
@@ -7,10 +7,6 @@ import { seedDefaultAdmin } from "./utils/seedAdmin.js";
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
-
 try {
   await sequelize.authenticate();
   console.log("Database connected");
