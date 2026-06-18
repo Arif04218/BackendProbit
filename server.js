0da49f4c0b7cb72ecffe816b5a9e997b00da2fa2
Arif Rahman Hakim <162030147+Arif04218@users.noreply.github.com>
2026-06-19 05:35:04 +0700

    Update server.js

--- server.js
+++ server.js
@@ -7,6 +7,10 @@ import { seedDefaultAdmin } from "./utils/seedAdmin.js";
 
 const PORT = process.env.PORT || 5000;
 
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
+
 try {
   await sequelize.authenticate();
   console.log("Database connected");
